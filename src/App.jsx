252b81574eb800f86b5be180daab993b8b424b45
Shaxzod <132588075+shaxzocoder@users.noreply.github.com>
2024-01-09 21:44:28 +0500

    Pages + title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { useEffect,  useState } from 'react';
 import { Route , Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Speaking from './Pages/Speaking/index';
@@ -6,7 +8,25 @@ import Social from './Pages/Social/index';
 import Playlist from './Pages/Playlist/index';
 import Home from './Pages/Home/index';
 
+
+
+const titleMap = [
+  { path: '/', title: 'Shaxzod - Blog' },
+  { path: '/speaking', title: 'Shaxzod - Speaking' },
+  { path: '/social', title: 'My Social medias' },
+  { path: '/playlists', title: 'My Social Playlist' }
+];
 const App = () => {
+  const [pageTitle, setPageTitle] = useState('Home');
+  const curLoc = useLocation();
+
+  useEffect(() => {
+    const curTitle = titleMap.find(item => item.path === curLoc.pathname);
+    if (curTitle && curTitle.title) {
+      setPageTitle(curTitle.title);
+      document.title = curTitle.title;
+    }
+  }, [curLoc]);
   return (
     <div className="main">
       <Navbar/>

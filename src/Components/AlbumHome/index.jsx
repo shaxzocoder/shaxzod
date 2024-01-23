466a5bf3380d0fc148ba20674e92a5ebe2a9b5c3
Shaxzod <132588075+shaxzocoder@users.noreply.github.com>
2024-01-23 19:20:12 +0500

    album cover

--- src/Components/AlbumHome/index.jsx
+++ src/Components/AlbumHome/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RecoveryCover } from "../../assets";
 import "./style.css";
+import { FaArrowRightLong } from "react-icons/fa6";
 import { GridBackgroundDemo } from "../BackgroundBoxs";
 
 const AlbumAim = () => {
@@ -12,9 +13,10 @@ const AlbumAim = () => {
           <h1>I want to buy this  <span className="coral">album</span></h1>
           <p>
             I was highly influenced by this artist known as <span className="hero">Eminem</span>. And this
-            album is my <span className="hero">favourite one</span>.I would like to add this album to my
+            album is my <span className="hero">favourite one</span>. I would like to add this album to my
             collection.
           </p>
+          <a className="open__recovery" target="_blank" href="https://open.spotify.com/album/47BiFcV59TQi2s9SkBo2pb?si=_rNeyN87T4aWpfozdU_bPQ"> Open this on spotify <FaArrowRightLong  className="arrow__recovery"/></a>
         </div>
       </section>
     </GridBackgroundDemo>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -25,6 +25,7 @@ const Home = () => {
           </Link>
       </div>
     </div>
+    <AlbumAim/>
     </header>
   );
 };

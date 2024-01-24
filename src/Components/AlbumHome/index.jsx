bd49459fd0503157b97de732ee280cc400da462d
Shaxzod <132588075+shaxzocoder@users.noreply.github.com>
2024-01-24 21:06:34 +0500

    responsive album cover

--- src/Components/AlbumHome/index.jsx
+++ src/Components/AlbumHome/index.jsx
@@ -8,9 +8,9 @@ const AlbumAim = () => {
   return (
     <GridBackgroundDemo>
       <section className="albumsection">
-        <img className="" src={RecoveryCover} alt="" />
+        <img src={RecoveryCover} alt="" />
         <div>
-          <h1>I want to buy this  <span className="coral">album</span></h1>
+          <h1 className="prosta__gap">I want to buy this  <span className="coral">album</span></h1>
           <p>
             I was highly influenced by this artist known as <span className="hero">Eminem</span>. And this
             album is my <span className="hero">favourite one</span>. I would like to add this album to my

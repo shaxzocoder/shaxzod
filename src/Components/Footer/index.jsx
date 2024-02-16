3cab6b5f1248363df48ba70dc18126b4e79daff5
Shaxzod <132588075+shaxzocoder@users.noreply.github.com>
2024-02-16 22:50:39 +0500

    footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        
+    </footer>
+  );
+};
+
+export default Footer;
